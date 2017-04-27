3f2958aa313ab5d367cb3d7491b20491e0142841
Karol Sójko <karolsojko@gmail.com>
2017-04-27 22:06:15 +0200

    Add deleting paths

---
+++ paths/delete.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.delete = (event, context, callback) => {
+  const params = {
+    TableName: process.env.PATHS_TABLE,
+    Key: {
+      userId: event.pathParameters.userId,
+      id: event.pathParameters.id,
+    },
+  };
+
+  dynamoDb.delete(params, (error) => {
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t remove the path item.'));
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Credentials" : true
+      },
+      body: JSON.stringify({}),
+    };
+    callback(null, response);
+  });
+};
