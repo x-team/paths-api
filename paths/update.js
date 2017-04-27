41c23c16c295866646651a0cb129c1e6460bbbe7
Karol Sójko <karolsojko@gmail.com>
2017-04-27 22:13:25 +0200

    Add updating paths

---
+++ paths/update.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.update = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  if (typeof data.name !== 'string') {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t update the path item.'));
+    return;
+  }
+
+  const params = {
+    TableName: process.env.PATHS_TABLE,
+    Key: {
+      userId: event.pathParameters.userId,
+      id: event.pathParameters.id,
+    },
+    ExpressionAttributeNames: {
+      '#path_name': 'name',
+    },
+    ExpressionAttributeValues: {
+      ':name': data.name,
+      ':updatedAt': timestamp,
+    },
+    UpdateExpression: 'SET #path_name = :name, updatedAt = :updatedAt',
+    ReturnValues: 'ALL_NEW',
+  };
+
+  dynamoDb.update(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t update the path item.'));
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Credentials" : true
+      },
+      body: JSON.stringify(result.Attributes),
+    };
+
+    callback(null, response);
+  });
+};
