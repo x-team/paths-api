f8b810efb37be012c2505f043b6986d36e619b06
Karol Sójko <karolsojko@gmail.com>
2017-04-28 11:47:53 +0200

    Add creating goals in a path

---
+++ paths/goals/create.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  if (typeof data.name !== 'string' || typeof data.description !== 'string') {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t create the goal item.'));
+    return;
+  }
+
+  const params = {
+    TableName: process.env.GOALS_TABLE,
+    Item: {
+      id: uuid.v1(),
+      pathId: event.pathParameters.id,
+      name: data.name,
+      description: data.description,
+      icon: data.icon,
+      level: data.level,
+      order: data.order,
+      dueDate: data.dueDate,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
+  };
+
+  dynamoDb.put(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t create the goal item.'));
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Credentials" : true
+      },
+      body: JSON.stringify(params.Item),
+    };
+
+    callback(null, response);
+  });
+};
