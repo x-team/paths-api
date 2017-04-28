9e4652f96c67382c2865ad4c23a2adfd6ea39c36
Karol Sójko <karolsojko@gmail.com>
2017-04-28 10:32:39 +0200

    Add getting a single path

---
+++ paths/get.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.get = (event, context, callback) => {
+  const params = {
+    TableName: process.env.PATHS_TABLE,
+    Key: {
+      userId: event.pathParameters.userId,
+      id: event.pathParameters.id,
+    },
+  };
+
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch the path item.'));
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Credentials" : true
+      },
+      body: JSON.stringify(result.Item),
+    };
+
+    callback(null, response);
+  });
+};
