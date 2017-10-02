cef575a5edc1242b901a5186da470bc85cf3d5a2
Rafal Wilinski <raf.wilinski@gmail.com>
2017-10-02 13:21:06 +0200

    Refactor Steps Responses

--- paths/steps/create.js
+++ paths/steps/create.js
@@ -2,15 +2,17 @@
 
 const uuid = require('uuid');
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const response = require('../utils/response');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.create = (event, context, callback) => {
   const timestamp = new Date().getTime();
   const data = JSON.parse(event.body);
+
   if (typeof data.name !== 'string' || typeof data.description !== 'string') {
     console.error('Validation Failed');
-    callback(new Error('Couldn\'t create the step item.'));
+    callback(null, response.BadRequest('Couldn\'t create the step item.'));
     return;
   }
 
@@ -35,19 +37,10 @@ module.exports.create = (event, context, callback) => {
   dynamoDb.put(params, (error, result) => {
     if (error) {
       console.error(error);
-      callback(new Error('Couldn\'t create the step item.'));
+      callback(null, response.BadRequest('Couldn\'t create the step item.'));
       return;
     }
 
-    const response = {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin" : "*",
-        "Access-Control-Allow-Credentials" : true
-      },
-      body: JSON.stringify(params.Item),
-    };
-
-    callback(null, response);
+    callback(null, response.OK(params.Item));
   });
 };
