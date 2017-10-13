039458addcc3476f295c805e7cf6aeefc5c7ee1c
Rafal Wilinski <raf.wilinski@gmail.com>
2017-10-13 15:20:34 +0200

    Fix type comparison

--- services/dynamodb.js
+++ services/dynamodb.js
@@ -6,10 +6,10 @@ const DynamoService = (options) => {
   }
 
   return new AWS.DynamoDB.DocumentClient({
-    region: process.env.LOCAL === 1 ? 'localhost' : (
+    region: process.env.LOCAL === "1" ? 'localhost' : (
       options.region || 'us-east-1'
     ),
-    endpoint: process.env.LOCAL === 1  ? 'http://localhost:8000' : undefined,
+    endpoint: process.env.LOCAL === "1" ? 'http://localhost:8000' : undefined,
   });
 }
 
